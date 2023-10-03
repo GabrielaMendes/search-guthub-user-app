222b8ebfff6645810c4f5051f20ffa75e1814a97
Gabriela <gdemelo.mendes@gmail.com>
2023-10-03 17:55:51 -0300

    Created project, added tailwind css and custom colors and font

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		fontFamily: {
+			"space-mono": "Space Mono, monospace",
+		},
+		fontSize: {
+			sm: ["0.8125rem", "1.25rem"], // 13px 20px
+			base: ["0.9375rem", "1.5625rem"], // 15px 25px
+			lg: ["1rem", "1.5rem"], // 16px 24px
+			xl: ["1.375rem", "2.0625rem"], // 22px 33px
+			"2xl": ["1.625rem", "2.375rem"], // 26px 38px
+		},
+		extend: {
+			colors: {
+				"primary-blue": "#0079ff",
+				"secondary-blue": "#60abff",
+				"bluish-gray": "#697c9a",
+				"grayish-blue": "#4b6a9b",
+				"dark-gray": "#2b3442",
+				"very-light-blue": "#f6f8ff",
+				"off-white": "#fefefe",
+				"almost-black": "#141d2f",
+				"very-dark-blue": "#1e2a47",
+				"error-red": "#f74646",
+			},
+		},
+	},
+	plugins: [],
+};
